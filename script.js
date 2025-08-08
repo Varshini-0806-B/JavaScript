17c09af9dd5b6f39f345b6d39b2668c11b1089e9
Varshini-0806-B <bollampallyvarshini@gmail.com>
2025-08-08 15:30:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Welcome to MRECW");
+document.write("Hello, Good Morning");
+document.write("<h1>Welcome to JavaScript</h1>");
+//var, let, const
+var Varsh = 10;
+document.write(Varsh);
+let V = 20;
+document.write(V);
+console.log("Varsh is " + Varsh);
